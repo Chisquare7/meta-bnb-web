611b7a4787d55abea9e87c7bbc1a296880150d2e
Chisquare7 <chijiokechibuike.f@gmail.com>
2022-11-11 11:01:00 +0100

    feat: Add the layout structure to the header and footer file

---
+++ src/components/footer.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+		<div>
+			<div className="sect__container">
+				<div>
+					<div>
+						<div></div>
+						<div></div>
+						<div></div>
+						<div></div>
+					</div>
+                    <div>
+                        <p></p>
+                    </div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default footer
---
+++ src/components/header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const header = () => {
+  return (
+    <div>
+      <div className='sect__container'>
+        <div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default header
