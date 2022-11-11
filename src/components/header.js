468878e43600301c1a14a62e94cacf721dc8d5e3
Chisquare7 <chijiokechibuike.f@gmail.com>
2022-11-11 12:49:23 +0100

    feat: Import Emotion packages, MenuIcon and CloseIcon from material UI

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 import { Link } from 'react-router-dom'
 import metabnb_logo from '../assets/metabnb__logo.png'
 
@@ -10,8 +12,14 @@ const Header = () => {
 					<div>
                       <Link><img src={ metabnb_logo } alt="main metabnb logo" /></Link>
 					</div>
+                    <div className='menu__content'>
+                        <MenuIcon className='menu__icon' />
+                    </div>
 					<nav>
 						<ul>
+                            <div className='close__content'>
+                                <CloseIcon className='close__icon' />
+                            </div>
 							<li>
 								<Link>Home</Link>
 							</li>
