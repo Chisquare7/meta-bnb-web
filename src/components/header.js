331b73f12f445615a69c712f6693731978778f8f
Chisquare7 <chijiokechibuike.f@gmail.com>
2022-11-12 02:59:14 +0100

    refactor: Restructure the layout of the Homepage

--- src/components/header.js
+++ src/components/header.js
@@ -14,55 +14,46 @@ const Header = () => {
     }
 
   return (
-		<div>
-			<div>
-					<div className='header__flex'>
-						<div className='main__logoContainer'>
-							<Link>
-								<img src={metabnb_logo} alt="main metabnb logo" className='metabnb__logo' />
-							</Link>
-						</div>
-						<div className="menu__content">
-							<MenuIcon className="menu__icon" onClick={showMenu} />
-						</div>
-						<nav className={active ? 'slider active' : 'slider'}>
-							<ul>
-								<div className="nav__middle">
-									<div className="close__content">
-										<CloseIcon className="close__icon" onClick={showMenu} />
-									</div>
-									<li className="nav__items">
-										<Link className="nav__text">Home</Link>
-									</li>
-									<li className="nav__items">
-										<Link className="nav__text">Place to stay</Link>
-									</li>
-									<li className="nav__items">
-										<Link className="nav__text">NFTs</Link>
-									</li>
-									<li className="nav__items">
-										<Link className="nav__text">Community</Link>
-									</li>
-								</div>
-								<div className='btn__container'>
-									<li className="nav__items">
-										<Link>
-											<button className='btn__connect'>Connect wallet</button>
-										</Link>
-									</li>
-								</div>
-							</ul>
-							{/* <ul>
-								<li>
-									<Link>
-										<button>Connect wallet</button>
-									</Link>
-								</li>
-							</ul> */}
-						</nav>
-					</div>
+		
+		<div className='header__flex'>
+			<div className='main__logoContainer'>
+				<Link>
+					<img src={metabnb_logo} alt="main metabnb logo" className='metabnb__logo' />
+				</Link>
 			</div>
+			<div className="menu__content">
+				<MenuIcon className="menu__icon" onClick={showMenu} />
+			</div>
+			<nav className={active ? 'slider active' : 'slider'}>
+				<ul>
+					<div className="nav__middle">
+						<div className="close__content">
+							<CloseIcon className="close__icon" onClick={showMenu} />
+						</div>
+						<li className="nav__items">
+							<Link className="nav__text">Home</Link>
+						</li>
+						<li className="nav__items">
+							<Link className="nav__text">Place to stay</Link>
+						</li>
+						<li className="nav__items">
+							<Link className="nav__text">NFTs</Link>
+						</li>
+						<li className="nav__items">
+							<Link className="nav__text">Community</Link>
+						</li>
+					</div>
+					<div className='btn__container'>
+						<li className="nav__items">
+							<Link>
+								<button className='btn__connect'>Connect wallet</button>
+							</Link>
+						</li>
+					</div>
+				</ul>
+			</nav>
 		</div>
+
 	);
 }
 
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -5,51 +5,47 @@ import Header from "../components/Header";
 const Homepage = () => {
   return (
     <div>
-      <div>
         <header className="header__container">
             <div className='sect__container'>
                 <Header />
             </div>
         </header>
-        <main>
-            <section className='hero__section'>
-                <div className='sect__container'>
-                    <div>
-                        <div className='hero__left'></div>
-                        <div className='hero__right'></div>
-                    </div>
+        <section className='hero__section'>
+            <div className='sect__container'>
+                <div>
+                    <div className='hero__left'></div>
+                    <div className='hero__right'></div>
                 </div>
-            </section>
-            <section className='layout__one'>
-                <div className='sect__container'>
-                    <div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                    </div>
-                </div>
-            </section>
-            <section className='layout__two'>
-                <div className='sect__container'>
-                    {/* <h2></h2> */}
+            </div>
+        </section>
+        <section className='layout__one'>
+            <div className='sect__container'>
+                <div>
+                    <div></div>
+                    <div></div>
                     <div></div>
                 </div>
-            </section>
-            <section className='layout__three'>
-                <div className='sect__container'>
-                    <div>
-                        <div className='layout__three-left'></div>
-                        <div className='layout__three-right'></div>
-                    </div>
+            </div>
+        </section>
+        <section className='layout__two'>
+            <div className='sect__container'>
+                {/* <h2></h2> */}
+                <div></div>
+            </div>
+        </section>
+        <section className='layout__three'>
+            <div className='sect__container'>
+                <div>
+                    <div className='layout__three-left'></div>
+                    <div className='layout__three-right'></div>
                 </div>
-            </section>
-        </main>
+            </div>
+        </section>
         <footer className="sect__footer">
             <div className='sect__container'>
                 <Footer />
             </div>
         </footer>
-      </div>
     </div>
   )
 }
