c0424b49355a2f79873d4cdbc356a5c21e37bac6
Chisquare7 <chijiokechibuike.f@gmail.com>
2022-11-14 11:10:28 +0100

    feat: Complete the modal component of all views with CSS styling

--- src/components/header.js
+++ src/components/header.js
@@ -3,10 +3,8 @@ import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
 import { Link } from 'react-router-dom'
 import metabnb_logo from '../assets/metabnb__logo.png'
-import modal__metatask from '../assets/modal__metatask.png'
-import modal__rightarrow from '../assets/modal__rightarrow.png'
-import modal__wallet from '../assets/modal__wallet.png'
-import X from '../assets/x.png'
+import Modal from './Modal';
+
 
 const Header = () => {
 
@@ -15,7 +13,9 @@ const Header = () => {
 
     const showMenu = () => {
         setActive(!active)
-    }
+	}
+	
+	const [openModal, setOpenModal] = useState(false);
 
   return (
 		<div className="for__modal">
@@ -56,74 +56,14 @@ const Header = () => {
 						<div className="btn__container">
 							<li className="nav__items">
 								<Link>
-									<button className="btn__connect">Connect wallet</button>
+									<button className="btn__connect" onClick={() => setOpenModal(true)}>Connect wallet</button>
 								</Link>
 							</li>
 						</div>
 					</ul>
 				</nav>
 			</div>
-			<div className="modal__container">
-				<div className="modal__section">
-					<div className="modal__popup">
-						<div className="modal__content-top">
-							<div className="modal__content-center">
-								<div className="modal__flex">
-									<h1 className="modal__heading">Connect Wallet</h1>
-									<img src={X} alt="close icon" className="modal__close" />
-								</div>
-							</div>
-						</div>
-						<div className="modal__content-bottom">
-							<div className="modal__content-center">
-								<div className="modal__subtext">
-									<p className="modal__subhead">
-										Choose your preferred wallet:
-									</p>
-								</div>
-								<div className="modal__link-box">
-									<div className="modal__link-flex">
-										<div className="modal__box-flex">
-											<img
-												src={modal__metatask}
-												alt="modal metatask icon"
-												className="modal__icons"
-											/>
-											<p className="modal__text">Metamask</p>
-										</div>
-										<div className="modal__arrow-content">
-											<img
-												src={modal__rightarrow}
-												alt="modal right arrow"
-												className="modal__arrow"
-											/>
-										</div>
-									</div>
-								</div>
-								<div className="modal__link-box">
-									<div className="modal__link-flex">
-										<div className="modal__box-flex">
-											<img
-												src={modal__wallet}
-												alt="modal wallet connect"
-												className="modal__icons"
-											/>
-											<p className="modal__text">WalletConnect</p>
-										</div>
-										<div className="modal__arrow-content">
-											<img
-												src={modal__rightarrow}
-												alt="modal right arrow"
-												className="modal__arrow"
-											/>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
+			<Modal open={openModal} onClose={() => setOpenModal(false)} />
 		</div>
 	);
 }
